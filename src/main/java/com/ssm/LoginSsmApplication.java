437d3606df2a5bb81c4f0788382c2334fb408366
XiaoSong <benz020121@gmail.com>
2023-04-02 13:54:30 +0800

    Initial commit

---
+++ src/main/java/com/ssm/LoginSsmApplication.java
@@ -0,0 +1,13 @@
+package com.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSsmApplication.class, args);
+    }
+
+}
